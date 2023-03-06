2eff358a86403fb5232d1d245bc7c59ab9db436b
aayubxon701 <aayubxon701@gmail.com>
2023-03-06 22:48:04 +0900

    Footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Container,
+  Wrapper,
+  MainItems,
+  LogoItems,
+  Logo,
+  LogoTitle,
+  LogoText,
+} from "./style";
+import logos from "../../Assets/Logo/logo.svg";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <MainItems>
+          <LogoItems>
+            <Logo src={logos} alt="" />
+            <LogoTitle>Funkling</LogoTitle>
+            <LogoText>
+            Funking is the a popular Ecommerce site. Lorem ipsum dolor sit amet,
+            consectetur!
+          </LogoText>
+          </LogoItems>
+        </MainItems>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Footer/style.js
+++ src/Components/Footer/style.js
@@ -1,3 +1,54 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-styled
+const common = css`
+  font-family: "Rubik";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: #555555;
+`;
+
+const Container = styled.div`
+  display: flex;
+  margin-top:80px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const LogoItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction:column;
+`;
+const MainItems = styled.div`
+  display: flex;
+  align-items:center;
+  flex-direction:column; 
+`;
+
+const Logo = styled.img`
+  width: 45px;
+  height: 45px;
+`;
+
+const LogoTitle = styled.p`
+  ${common}
+  margin:0;
+  padding: 0;
+  margin-left: 10px;
+`;
+
+const LogoText = styled.p`
+  ${common}
+  margin:0;
+  padding: 0;
+  font-size: 16px;
+  width:70%;
+`;
+
+export { Container, Wrapper, MainItems, LogoItems, Logo,LogoText, LogoTitle };
